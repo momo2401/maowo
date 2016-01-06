c8d764a2b1f6f1ebb0b3e3b4be404b8492e6fbf4
AriesM <cuimiao_miao@163.com>
2016-01-06 13:45:19 +0800

    重新添加

---
+++ MainProject/MainProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MainProject
+//
+//  Created by 七七的移动城堡 on 15/12/30.
+//  Copyright (c) 2015年 七七的移动城堡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MainProject/MainProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MainProject
+//
+//  Created by 七七的移动城堡 on 15/12/30.
+//  Copyright (c) 2015年 七七的移动城堡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
